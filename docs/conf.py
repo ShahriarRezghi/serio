bb3ad03e8df3ec8508c3116ca3937d9909fd18f9
Shahriar <shahriar.rezghi.sh@google.com>
2026-06-21 23:57:06 +0300

    Updated config files

--- docs/conf.py
+++ docs/conf.py
@@ -3,20 +3,27 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import subprocess
+
+conf_path = os.path.dirname(__file__)
+subprocess.check_call(["doxygen", "Doxyfile"], cwd=conf_path)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Serio'
-copyright = '2024, Shahriar Rezghi'
+copyright = '2019-2026, Shahriar Rezghi'
 author = 'Shahriar Rezghi'
+release = '2026'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['breathe', 'sphinx_rtd_theme']
+extensions = ['breathe', 'sphinx_rtd_theme', 'sphinx_togglebutton']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 breathe_projects = {'Serio': '_build/xml/'}
 breathe_default_project = 'Serio'
@@ -26,3 +33,4 @@ breathe_default_project = 'Serio'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = '_static/logo-64.png'
